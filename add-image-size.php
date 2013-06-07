acbe64098d0a399e1419066b8d12c9aced661ca0
Kailey Lampert <trepmal@gmail.com>
2013-06-07 10:42:53 -0700

    first commit

---
+++ add-image-size.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * Plugin Name: Add Image Size
+ * Plugin URI: GUI for add_image_size()
+ * Description:
+ * Version:
+ * Author: Kailey Lampert
+ * Author URI: kaileylampert.com
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * TextDomain: add-image-size
+ * DomainPath:
+ * Network:
+ */
+
+// @todo Localize
+// @todo ajax admin
+// @todo actually create the image sizes
+
+$add_image_size = new Add_Image_Size();
+
+class Add_Image_Size {
+
+	var $page_name;
+
+	function __construct() {
+		add_action( 'admin_init', array( &$this, 'register_options' ) );
+		add_action( 'admin_menu', array( &$this, 'menu' ) );
+
+		$sizes = get_option( 'ais-images', false );
+		if ( $sizes ) foreach ( $sizes as $image_deets ) {
+			add_image_size( $image_deets['name'], $image_deets['width'], $image_deets['height'], $image_deets['crop'] );
+		}
+	}
+
+	function register_options() {
+		register_setting( 'add-image-size', 'ais-images', array( &$this, 'sanitize' ) );
+
+		// delete_option( 'ais-images' );
+		add_settings_section( 'ais-section', 'Image Size Properties', function() { echo ''; }, $this->page_name );
+		add_settings_field( 'ais-image-row', 'Images:', array( &$this, 'field' ), $this->page_name, 'ais-section', get_option( 'ais-images', false ) );
+	}
+
+	function sanitize( $input ) {
+		$sample = array(
+			array(
+				'name' => 'itty-bitty',
+				'width' => 50,
+				'height' => 50,
+				'crop' => 1
+			),
+			array(
+				'name' => 'itty-bitty',
+				'width' => 50,
+				'height' => 50,
+				'crop' => 1
+			),
+		);
+		$newinput = array();
+		foreach( $input as $k => $image_deets ) {
+
+			extract( array_map( 'trim', $image_deets ) );
+
+			if ( empty( $name ) || empty( $width ) || empty( $height ) ) {
+				unset( $input[ $k ] ); continue;
+			}
+
+			$name = sanitize_title( $name );
+			$width = (int) $width;
+			$height = (int) $height;
+			$crop = (bool) $crop;
+
+			if ( $width < 1 || $height < 1 ) {
+				unset( $input[ $k ] ); continue;
+			}
+
+			unset( $input[ $k ] );
+			$newinput[ uniqid() ] = compact( 'name', 'width', 'height', 'crop' );
+		}
+
+		return $newinput;
+	}
+
+	function field( $args ) {
+		// printer( $args );
+		if ( $args != false ) foreach( $args as $id => $params ) {
+			?>
+		<input type="text" name="ais-images[<?php echo $id; ?>][name]" value="<?php echo $params['name']; ?>" />
+		<input type="text" name="ais-images[<?php echo $id; ?>][width]" value="<?php echo $params['width']; ?>" />
+		<input type="text" name="ais-images[<?php echo $id; ?>][height]" value="<?php echo $params['height']; ?>" />
+		<input type="hidden" name="ais-images[<?php echo $id; ?>][crop]" value="0" />
+		<input type="checkbox" name="ais-images[<?php echo $id; ?>][crop]" value="1" <?php checked( $params['crop'] ); ?> />
+			<?php
+		}
+		$key_id = 'blah';
+		?>
+		<input type="text" name="ais-images[<?php echo $key_id; ?>][name]" value="" />
+		<input type="text" name="ais-images[<?php echo $key_id; ?>][width]" value="" />
+		<input type="text" name="ais-images[<?php echo $key_id; ?>][height]" value="" />
+		<input type="hidden" name="ais-images[<?php echo $key_id; ?>][crop]" value="0" />
+		<input type="checkbox" name="ais-images[<?php echo $key_id; ?>][crop]" value="1" />
+		<?php
+	}
+
+	function menu() {
+		$this->page_name = add_options_page( __( 'Image Sizes', 'add-image-size' ), __( 'Image Sizes', 'add-image-size' ), 'edit_posts', __CLASS__, array( &$this, 'page' ) );
+	}
+
+	function page() {
+		?><div class="wrap">
+		<h2><?php _e( 'Image Sizes', 'add-image-size' ); ?></h2>
+		<p>Create additional named image sizes for easy use</p>
+		<form method="post" action="options.php">
+		<?php
+		settings_fields( 'add-image-size' );
+		do_settings_sections( $this->page_name );
+		submit_button();
+		?>
+		</form>
+		<?php
+
+		?>
+		</div><?php
+	}
+
+}
+if ( ! function_exists( 'printer') ) {
+	function printer( $input ) {
+		echo '<pre>' . print_r( $input, true ) . '</pre>';
+	}
+}
+//eof
