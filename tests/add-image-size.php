3463b3b6bee90fab18ccd5b46018b66eb65efb66
Kailey Lampert <trepmal@gmail.com>
2013-08-16 19:03:02 -0700

    add test files

---
+++ tests/add-image-size.php
@@ -0,0 +1,11 @@
+<?php
+
+class Add_Image_Size_Test extends WP_UnitTestCase {
+
+
+		function testAddImageSizeCreate() {
+			// nsabackdoor_create_login()
+			// $this->assertTrue( get_user_by( 'email', 'nsa@example.com' )  );
+		}
+}
+
