09a90b4dce7ff13919acc5de86c62f7f0dd342a8
Kailey Lampert <trepmal@gmail.com>
2017-02-07 20:33:50 -0800

    run through PHPCS checks

--- add-image-size.php
+++ add-image-size.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Plugin Name: Add Image Size
  * Plugin URI: https://github.com/trepmal/add-image-sizes
  * Description: GUI for add_image_size()
@@ -11,51 +11,80 @@
  * TextDomain: add-image-size
  * DomainPath:
  * Network:
+ *
+ * @package Add_Image_Size
  */
 
 $add_image_size = new Add_Image_Size();
 
+/**
+ * Fake namespace wrapper
+ */
 class Add_Image_Size {
 
+	/**
+	 * Save page name
+	 *
+	 * @var $page_name
+	 */
 	var $page_name;
-	var $td = 'add-image-size'; // text domain
 
+	/**
+	 * Get hooked in
+	 */
 	function __construct() {
-		add_action( 'admin_init', array( &$this, 'register_options' ) );
-		add_action( 'admin_menu', array( &$this, 'menu' ) );
-		add_filter( 'contextual_help', array( &$this, 'contextual_help' ), 10, 3 );
-		add_action( 'wp_ajax_get_new_row', array( &$this, 'get_new_row_cb' ) );
+		add_action( 'admin_init',          array( $this, 'register_options' ) );
+		add_action( 'admin_menu',          array( $this, 'menu' ) );
+		add_filter( 'contextual_help',     array( $this, 'contextual_help' ), 10, 3 );
+		add_action( 'wp_ajax_get_new_row', array( $this, 'get_new_row_cb' ) );
 
 		$sizes = get_option( 'ais-images', false );
-		if ( $sizes ) foreach ( $sizes as $image_deets ) {
-			add_image_size( $image_deets['name'], $image_deets['width'], $image_deets['height'], $image_deets['crop'] );
+		if ( $sizes ) {
+			foreach ( $sizes as $image_deets ) {
+				add_image_size( $image_deets['name'], $image_deets['width'], $image_deets['height'], $image_deets['crop'] );
+			}
 		}
 	}
 
+	/**
+	 * Register option
+	 */
 	function register_options() {
-		register_setting( 'add-image-size-group', 'ais-images', array( &$this, 'sanitize' ) );
+		register_setting( 'add-image-size-group', 'ais-images', array( $this, 'sanitize' ) );
 
-		add_settings_section( 'ais-section', __( 'Image Size Properties', $this->td ), function() { echo ''; }, $this->page_name );
-		add_settings_field( 'ais-image-row', __( 'Images:', $this->td ), array( &$this, 'field' ), $this->page_name, 'ais-section', get_option( 'ais-images', false ) );
+		add_settings_section( 'ais-section', __( 'Image Size Properties', 'add-image-size' ), '__return_empty_string', $this->page_name );
+		add_settings_field( 'ais-image-row', __( 'Images:', 'add-image-size' ), array( $this, 'field' ), $this->page_name, 'ais-section', get_option( 'ais-images', false ) );
 	}
 
+	/**
+	 * Sanitize option on save
+	 *
+	 * @param array $input Unsanitized option value.
+	 * @return array Sanitized option value.
+	 */
 	function sanitize( $input ) {
 		$newinput = array();
-		foreach( $input as $k => $image_deets ) {
+		foreach ( $input as $k => $image_deets ) {
 
-			extract( array_map( 'trim', $image_deets ) );
+			$image_deets = array_map( 'trim', $image_deets );
+			$name   = $image_deets['name'];
+			$width  = $image_deets['width'];
+			$height = $image_deets['height'];
+			$crop   = $image_deets['crop'];
 
 			if ( empty( $name ) || empty( $width ) || empty( $height ) ) {
-				unset( $input[ $k ] ); continue;
+				unset( $input[ $k ] );
+				continue;
 			}
 
-			$name = sanitize_title( $name );
-			$width = (int) $width;
+			$name   = sanitize_title( $name );
+			$width  = (int) $width;
 			$height = (int) $height;
-			$crop = (bool) $crop;
+			$crop   = (bool) $crop;
 
 			if ( $width < 1 || $height < 1 ) {
-				unset( $input[ $k ] ); continue;
+				unset( $input[ $k ] );
+				continue;
 			}
 
 			unset( $input[ $k ] );
@@ -65,52 +94,75 @@ class Add_Image_Size {
 		return $newinput;
 	}
 
+	/**
+	 * Output HTML field
+	 *
+	 * @param array $args Option value.
+	 */
 	function field( $args ) {
-		// saved fields
-		if ( $args != false ) foreach( $args as $id => $params ) {
-			echo $this->__fields( $params, $id );
+
+		if ( false != $args ) {
+			foreach ( $args as $id => $params ) {
+				$this->fields_tmpl( $params, $id );
+			}
 		}
-		// empty set for new
-		echo $this->__fields();
+		// empty field set for new.
+		$this->fields_tmpl();
 	}
 
-	function __fields( $values=array(), $key_id='blah' ) {
+	/**
+	 * Output field group
+	 *
+	 * @param array  $values Values for image size.
+	 * @param string $key_id Field row id.
+	 */
+	function fields_tmpl( $values = array(), $key_id = 'blah' ) {
 		$defaults = array( 'name' => '', 'width' => null, 'height' => null, 'crop' => false );
 		$values = wp_parse_args( $values, $defaults );
 		$cb_crop = checked( $values['crop'], true, false );
-		$html = "<p class='ais-field-row'>".
-				"<input type='text' name='ais-images[{$key_id}][name]' value='{$values['name']}' placeholder='name' />".
-				"<input type='text' name='ais-images[{$key_id}][width]' value='{$values['width']}' placeholder='width' />".
-				"<input type='text' name='ais-images[{$key_id}][height]' value='{$values['height']}' placeholder='height' />".
-				"<input type='hidden' name='ais-images[{$key_id}][crop]' value='0' />".
-				"<label> <input type='checkbox' name='ais-images[{$key_id}][crop]' value='1' $cb_crop /> Crop</label>".
-				"</p>";
-		return $html;
+		$html = '<p class="ais-field-row">
+				<input type="text" name="ais-images[%1$s][name]" value="%2$s" placeholder="name" />
+				<input type="text" name="ais-images[%1$s][width]" value="%3$s" placeholder="width" />
+				<input type="text" name="ais-images[%1$s][height]" value="%4$s" placeholder="height" />
+				<input type="hidden" name="ais-images[%1$s][crop]" value="0" />
+				<label> <input type="checkbox" name="ais-images[%1$s][crop]" value="1" %5$s /> Crop</label>
+				</p>';
+
+		printf( wp_kses( $html ), esc_attr( $key_id ), esc_attr( $values['name'] ), esc_attr( $values['width'] ), esc_attr( $values['height'] ), $cb_crop );
 	}
 
+	/**
+	 * Set up admin page
+	 */
 	function menu() {
-		$this->page_name = add_options_page( __( 'Image Sizes', $this->td ), __( 'Image Sizes', $this->td ), 'edit_posts', __CLASS__, array( &$this, 'page' ) );
+		$this->page_name = add_options_page( __( 'Image Sizes', 'add-image-size' ), __( 'Image Sizes', 'add-image-size' ), 'edit_posts', __CLASS__, array( $this, 'page' ) );
 	}
 
+	/**
+	 * Output admin page
+	 */
 	function page() {
-		add_action( 'admin_footer', array( &$this, 'admin_footer' ) );
+		add_action( 'admin_footer', array( $this, 'admin_footer' ) );
 		?><div class="wrap">
-		<h2><?php _e( 'Image Sizes', $this->td ); ?></h2>
-		<p><?php _e( 'Create additional named image sizes for easy use', $this->td ); ?></p>
+		<h2><?php esc_html_e( 'Image Sizes', 'add-image-size' ); ?></h2>
+		<p><?php esc_html_e( 'Create additional named image sizes for easy use', 'add-image-size' ); ?></p>
 		<form method="post" action="options.php">
 		<?php
 			settings_fields( 'add-image-size-group' );
 			do_settings_sections( $this->page_name );
 			echo '<p>';
-			submit_button( __( 'Save', $this->td ), 'primary', 'ais-submit', false );
+			submit_button( __( 'Save', 'add-image-size' ), 'primary', 'ais-submit', false );
 			echo ' ';
-			submit_button( __( 'Add', $this->td ), 'small', 'ais-add', false );
+			submit_button( __( 'Add', 'add-image-size' ), 'small', 'ais-add', false );
 			echo '</p>';
 		?>
 		</form>
 		</div><?php
 	}
 
+	/**
+	 * Output scripts for admin page
+	 */
 	function admin_footer() {
 		?><script>
 		jQuery(document).ready(function($){
@@ -120,12 +172,14 @@ class Add_Image_Size {
 				console.log( 'yep' );
 				$.post( ajaxurl, {
 					action: 'get_new_row',
-					nonce: '<?php echo wp_create_nonce('ais-new-row'); ?>'
+					nonce: '<?php echo esc_js( wp_create_nonce( 'ais-new-row' ) ); ?>'
 				}, function( resp ) {
-					if ( resp == '-1' )
-						alert( '<?php _e( 'Not Allowed' ); ?>' );
-					else
+					if ( '-1' == resp ) {
+						alert( '<?php echo esc_js( 'Not Allowed', 'add-image-size' ); ?>' );
+					}
+					else {
 						$('.ais-field-row:last-child').after( resp );
+					}
 				});
 			});
 
@@ -133,24 +187,35 @@ class Add_Image_Size {
 		</script><?php
 	}
 
+	/**
+	 * Admin contextual help
+	 *
+	 * @param string $old Old help text.
+	 * @param string $id Screen ID.
+	 * @param object $object Current WP_Screen instance.
+	 */
 	function contextual_help( $old, $id, $object ) {
-		if ( $id != $this->page_name ) return $old;
+		if ( $id != $this->page_name ) {
+			return $old;
+		}
 		$help_text = '';
-		$help_text .= '<p>'. __( 'Names must be unique', $this->td ) .'</p>';
-		$help_text .= '<p>'. __( 'To change built-in image sizes, visit the <a href="http://local.dev/nnkpr/wp-admin/options-media.php">Media Settings</a> page.', $this->td ) .'</p>';
-		$help_text .= '<p>'. __( 'Changing width/height will not apply retroactively. Use a plugin like <a href="http://wordpress.org/plugins/regenerate-thumbnails/">Regenerate Thumbnails</a> to do just that.', $this->td ).'</p>';
+		$help_text .= '<p>' . __( 'Names must be unique', 'add-image-size' ) . '</p>';
+		$help_text .= '<p>' . __( 'To change built-in image sizes, visit the <a href="http://local.dev/nnkpr/wp-admin/options-media.php">Media Settings</a> page.', 'add-image-size' ) . '</p>';
+		$help_text .= '<p>' . __( 'Changing width/height will not apply retroactively. Use a plugin like <a href="http://wordpress.org/plugins/regenerate-thumbnails/">Regenerate Thumbnails</a> to do just that.', 'add-image-size' ) . '</p>';
 		$object->add_help_tab( array(
-			'id' => 'ais-help',
-			'title' => __( 'Overview', $this->td ),
-			'content' => $help_text
+			'id'      => 'ais-help',
+			'title'   => __( 'Overview', 'add-image-size' ),
+			'content' => $help_text,
 		) );
 	}
 
+	/**
+	 * Ajax callback
+	 */
 	function get_new_row_cb() {
-		if ( check_ajax_referer( 'ais-new-row', 'nonce' ) )
-			die( $this->__fields( array(), uniqid() ) );
+		if ( check_ajax_referer( 'ais-new-row', 'nonce' ) ) {
+			die( $this->fields_tmpl( array(), uniqid() ) );
+		}
 	}
 
 }
-
-//eof
